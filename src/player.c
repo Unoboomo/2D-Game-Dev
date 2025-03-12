23cd7b723971403e0d4dbfd16532048feac7ff3a
Unoboomo <jddjuch@gmail.com>
2025-03-12 02:20:24 -0400

    added crouch jumping, made it so you wont crouch jump when jumping out of a groundpound recovery

--- src/player.c
+++ src/player.c
@@ -65,12 +65,13 @@ void player_think(Entity* self) {
 	int move_direction;
 	float acceleration;
 	float friction;
+	float jump_velocity;
 
 	if (!self|| !self->data) {
 		return;
 	}
 	data = (PlayerEntityData*)self->data;
-
+	jump_velocity = JUMP_VELOCITY;
 
 	if (self->physics->x_world_collision) { //collision with a wall
 		data->last_x_collision_dir = self->physics->x_world_collision; //save the last collision direction
@@ -137,7 +138,7 @@ void player_think(Entity* self) {
 	else {
 		self->physics->running = 0;
 	}
-	if (gfc_input_command_down("down") && self->physics->grounded && !self->physics->override_horizontal_velocity_cap) {// player starts crouching
+	if (gfc_input_command_down("down") && self->physics->grounded && !self->physics->override_horizontal_velocity_cap && !data->ground_pound_recovery) {// player starts crouching
 		self->physics->bounds.h /= 2;
 		self->physics->bounds.y += self->physics->bounds.h;
 		self->physics->override_horizontal_velocity_cap = CROUCH_MAX_VELOCITY;
@@ -146,6 +147,7 @@ void player_think(Entity* self) {
 	}
 	if (data->crouching) {
 		self->physics->running = 0;
+		jump_velocity = CROUCH_JUMP_VELOCITY;
 		if (!gfc_input_command_down("down")) {
 
 			//resize hitbox to normal
@@ -185,11 +187,11 @@ void player_think(Entity* self) {
 
 	if (gfc_input_command_pressed("jump")) {
 		if (self->physics->grounded) { //normal jump
-			self->physics->velocity.y = JUMP_VELOCITY;
+			self->physics->velocity.y = jump_velocity;
 		}
 		else if (self->physics->x_world_collision || data->wall_jump_buffer > 0) { //wall jump
 			self->physics->velocity.x = -data->last_x_collision_dir * WALL_JUMP_VELOCITY;
-			self->physics->velocity.y = JUMP_VELOCITY;
+			self->physics->velocity.y = jump_velocity;
 			data->wall_jump_buffer = 0;
 		}
 		else { //buffer a jump
@@ -197,12 +199,12 @@ void player_think(Entity* self) {
 		}
 	}
 	else if (self->physics->grounded && data->jump_buffer > 0) { //buffered jump
-			self->physics->velocity.y = JUMP_VELOCITY;
+			self->physics->velocity.y = jump_velocity;
 			data->jump_buffer = 0;
 	}
 	else if ((self->physics->x_world_collision || data->wall_jump_buffer > 0) && data->jump_buffer > 0) { //buffered wall jump
 		self->physics->velocity.x = -data->last_x_collision_dir * WALL_JUMP_VELOCITY;
-		self->physics->velocity.y = JUMP_VELOCITY;
+		self->physics->velocity.y = jump_velocity;
 		data->wall_jump_buffer = 0;
 		data->jump_buffer = 0;
 	}
